8b68dfae1f5b2a84d4381209943c33236c9bb66a
Josh Gross <joshuamgr@gmail.com>
2020-05-14 22:14:54 -0400

    Remove string interpolation

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import os
 name = os.getenv("INPUT_NAME")
 phrase = os.getenv("INPUT_PHRASE")
 
-print(f'{name} says {phrase}.\n')
+print(name + " says " + phrase + ".\n")
 
 print("::set-output name=name::Python\n")
 print("::set-output name=phrase::🐍🐍🐍\n")
